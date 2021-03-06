87371c9b5a3b4c32bccec8cda4c7c62e59c71b5d
Caio <caiovr13@gmail.com>
2021-03-06 16:20:40 -0300

    Initial Commit

---
+++ Gerador Randômico/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gerador Randômico
+//
+//  Created by Caio Fernandes on 06/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
