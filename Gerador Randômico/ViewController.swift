88a833e3f10880f8465b4d4336c5428604fb4b85
Caio <caiovr13@gmail.com>
2021-03-06 17:01:29 -0300

    Gerador Randômico de números de 1 a 10)

--- Gerador Randômico/ViewController.swift
+++ Gerador Randômico/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBAction func randomNumberLaber(_ sender: UIButton) {
+        randomNumberLabel.text = String(randomNumber(MIN: 1, MAX: 10))
+    }
+    @IBOutlet weak var randomNumberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func randomNumber(MIN: Int, MAX: Int)-> Int{
+        return Int(arc4random_uniform(UInt32(MAX-MIN)) + UInt32(MIN));
+    }
 
 }
 
